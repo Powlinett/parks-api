2d3cad818eff85e5fabf334381bf65084593d866
Pauline <pauline.eustachy@gmail.com>
2020-09-14 21:58:27 +0200

    add route and middleware to fetch specific parks

--- controllers/parks.js
+++ controllers/parks.js
@@ -1,8 +1,27 @@
-exports.getParks = (req, res, next) => {
+const Park = require('../models/park');
+
+exports.getParks = async (req, res, next) => {
+  const parks = await Park.find();
   res.json({
-    park: 'blabla',
-    address: 'somewhere'
+    parks: parks,
+    length: parks.length
   });
+};
+
+exports.getQueriedParks = async (req, res, next) => {
+  const query = req.query;
+  console.log(query);
+  if (Object.keys(query).length === 0) {
+    return res.status(404).json({
+      parks: [],
+      message: 'No park found, please add queries to URL'
+    });
+  }
+  const parks = await Park.find(query);
+  res.status(200).json({
+    parks: parks,
+    length: parks.length
+  })
 };
 
 exports.postPark = (req, res, next) => {
--- routes/parks.js
+++ routes/parks.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.get('/parks', parksController.getParks);
 
+router.get('/parks/search', parksController.getQueriedParks);
+
 router.post('/parks', parksController.postPark);
 
 module.exports = router
